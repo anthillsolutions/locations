2555367fb22f279815c407c57aac0e3ea43c146a
Pierre Repetto-Andipatin <pierre.repetto@gmail.com>
2017-02-17 12:56:34 +0100

    fix: lint and newer bulk save function

--- index.js
+++ index.js
@@ -41,12 +41,12 @@ function flush() {
     });
 }
 
-function saveLocation(){
-  if(process.env.BULK_SAVE > 0){	
-	saveMultiple(process.env.BULK_SAVE);
-  }else{
-	saveSimple();
-  };
+function saveLocation() {
+  if (process.env.BULK_SAVE > 0) {
+    saveMultiple(process.env.BULK_SAVE);
+  } else {
+    saveSimple();
+  }
 }
 
 function saveSimple() {
@@ -66,22 +66,22 @@ function saveSimple() {
     });
 }
 
-function saveMultiple(count){
+function saveMultiple(count) {
 
   var locations = [];
-  
-  // Creating locations with random coordinates and adding them to an array 
-  for(var i=0; i<count; i++){
-	var location = new Location({
-	  timestamp: new Date().getTime(),
+
+  // Creating locations with random coordinates and adding them to an array
+  for (var i = 0; i < count; i++) {
+    var location = new Location({
+      timestamp: new Date().getTime(),
       latitude: Math.round((Math.random() * 90) * 100) / 100 ,
       longitude: Math.round((Math.random() * 180) * 100) / 100,
-	});
-	locations[i] = location;
+    });
+    locations[i] = location;
   }
 
   // Bulk inserting the array of locations into the DB
-  Location.collection.insert(locations)
+  Location.insertMany(locations)
     .then(locations => {
       console.log(locations);
       process.exit(0);
@@ -89,6 +89,5 @@ function saveMultiple(count){
     .catch(err => {
       console.error(err);
       process.exit(1);
-    });;
+    });
 }
-
